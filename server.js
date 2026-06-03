5dad229f02b9eddabfd12a5aceefb64c181a91ee
Banpara401991 <lasousa@banparanet.com.br>
2026-06-03 14:13:54 -0300

    Update server.js

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ const respostaAtendente = await axios.post(
     `https://graph.facebook.com/v23.0/${PHONE_NUMBER_ID}/messages`,
     {
         messaging_product: 'whatsapp',
-        to: '559484028241',
+        to: '5594991591220',
         type: 'text',
         text: {
              body: 'TESTE ATENDENTE'
